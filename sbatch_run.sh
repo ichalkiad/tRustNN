db35a8a4ca75b8a332565c1366cc026b70580229
Ioannis Chalkiadakis <icha@robotarium.cm.cluster>
2017-06-01 13:58:50 +0100

    Start up mongo server

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-SBATCH --partition=defq
-#SBATCH --nodes 2
+SBATCH --partition=longq
+SBATCH --nodes 2
 SBATCH --mail-user=ic14@hw.ac.uk
 SBATCH --mail-type=ALL 
 
 #module purge 
 #module load gcc/5.2.0
 
+./home/icha/mongodb_3.4.4/bin/mongod --config mongodb.conf &
+
 #execute application
-python3 dynamic_lstm_TF.py
+python3 /home/icha/tRustNN/src/dynamic_lstm_TF.py with 'db="file"'
 
 # exit
 exit 0
