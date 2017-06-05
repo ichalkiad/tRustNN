e479384db4ac2c9bd1007be39b68e25d0fbd4219
Ioannis Chalkiadakis <icha@robotarium.cm.cluster>
2017-06-05 14:07:33 +0100

    Fix sbatch commands

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-SBATCH --partition=longq
-SBATCH --nodes 2
-SBATCH --mail-user=ic14@hw.ac.uk
-SBATCH --mail-type=ALL 
+#SBATCH --partition=longq
+#SBATCH --nodes 1
 
 #module purge 
-#module load gcc/5.2.0
+module load gcc/5.2.0
+#module load easybuild
+#module load lang/Python/3.5.2-foss-2016b
 
-./home/icha/mongodb_3.4.4/bin/mongod --config mongodb.conf &
+#./home/icha/mongodb_3.4.4/bin/mongod --config mongodb.conf &
 
 #execute application
-python3 /home/icha/tRustNN/src/dynamic_lstm_TF.py with 'db="file"'
+python3 /home/icha/tRustNN/src/dynamic_lstm_TF.py with 'db="file"' 'net_arch.lstm.n_units=10'
 
 # exit
 exit 0
