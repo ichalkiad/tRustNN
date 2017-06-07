b90fa82940a234a9be3f9aa3b4f103255d2c163e
Ioannis Chalkiadakis <icha@robotarium.cm.cluster>
2017-06-07 11:23:09 +0100

    Use python3.5

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#SBATCH --partition=longq
+#SBATCH --partition=defq
 #SBATCH --nodes 1
 
 #module purge 
-module load gcc/5.2.0
-#module load easybuild
-#module load lang/Python/3.5.2-foss-2016b
+#module load gcc/5.2.0
+module load easybuild
+module load lang/Python/3.5.2-foss-2016b
 
 #./home/icha/mongodb_3.4.4/bin/mongod --config mongodb.conf &
 
 #execute application
-python3 /home/icha/tRustNN/src/dynamic_lstm_TF.py with 'db="file"' 'net_arch.lstm.n_units=10'
+python3.5 /home/icha/tRustNN/src/dynamic_lstm_TF.py with 'db="file"' 
 
 # exit
 exit 0
