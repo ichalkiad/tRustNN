32460f02bb4b8f10c805b1ec5c64e345e7e15243
Ioannis Chalkiadakis <icha@robotarium.cm.cluster>
2017-06-09 12:59:28 +0100

    Always pass neet architecture, in case of more than lstm-fc-output give full param spec of extra layers

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --partition=defq
+#SBATCH --partition=longq
 #SBATCH --nodes 1
 
 #module purge 
@@ -8,10 +8,15 @@
 module load easybuild
 module load lang/Python/3.5.2-foss-2016b
 
-#./home/icha/mongodb_3.4.4/bin/mongod --config mongodb.conf &
-
 #execute application
-python3.5 /home/icha/tRustNN/src/dynamic_lstm_TF.py with 'db="file"' 
+python3.5 /home/icha/tRustNN/src/dynamic_lstm_TF.py with 'db="file"' 'run_id="baseline"' 
+
+python3.5 /home/icha/tRustNN/src/dynamic_lstm_TF.py with 'db="file"' 'run_id="base_20LSTM"' 'net_arch.lstm.n_units=20'
+
+python3 src/dynamic_lstm_TF.py with 'db="file"' 'run_id="2Layers_50-20LSTM"' 'net_arch.lstm.n_units=50' 'net_arch.lstm.return_seq=True' 'net_arch.lstm2 = {"n_units":30, "activation":"tanh", "inner_activation":"sigmoid", \
+"dropout":None, "bias":True, "weights_init":None, "forget_bias":1.0, "return_seq":False, "return_state":False, "initial_state":None, "dynamic":False, "trainable":True, "restore":True, "reuse":False, "scope":None\
+, "name":"lstm2"}' 'net_arch_layers=["lstm","lstm2","fc","output"]'
+
 
 # exit
 exit 0
