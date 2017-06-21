c4c1f9ddcf2f2355af397680393dff1089dbc67a
Yannis Chalkiadakis <ic14@hw.ac.uk>
2017-06-21 17:39:38 +0100

    Add sample clustering plot

---
+++ bokeh_vis/clustering.py
@@ -0,0 +1,103 @@
+import numpy as np
+np.random.seed(0)
+
+from bokeh.io import curdoc
+from bokeh.layouts import widgetbox, row, column
+from bokeh.models import ColumnDataSource, Select, Slider
+from bokeh.plotting import figure
+from bokeh.palettes import Spectral6
+
+from sklearn import cluster, datasets
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+
+
+def clustering(X, algorithm, n_clusters):
+    # normalize dataset for easier parameter selection
+    X = StandardScaler().fit_transform(X)
+
+    # estimate bandwidth for mean shift
+    bandwidth = cluster.estimate_bandwidth(X, quantile=0.3)
+
+    # connectivity matrix for structured Ward
+    connectivity = kneighbors_graph(X, n_neighbors=10, include_self=False)
+
+    # make connectivity symmetric
+    connectivity = 0.5 * (connectivity + connectivity.T)
+
+    # Generate the new colors:
+    if algorithm=='MiniBatchKMeans':
+        model = cluster.MiniBatchKMeans(n_clusters=n_clusters)
+
+    elif algorithm=='Birch':
+        model = cluster.Birch(n_clusters=n_clusters)
+
+    elif algorithm=='DBSCAN':
+        model = cluster.DBSCAN(eps=.2)
+
+    elif algorithm=='AffinityPropagation':
+        model = cluster.AffinityPropagation(damping=.9,
+                                            preference=-200)
+
+    elif algorithm=='MeanShift':
+        model = cluster.MeanShift(bandwidth=bandwidth,
+                                  bin_seeding=True)
+
+    elif algorithm=='SpectralClustering':
+        model = cluster.SpectralClustering(n_clusters=n_clusters,
+                                           eigen_solver='arpack',
+                                           affinity="nearest_neighbors")
+
+    elif algorithm=='Ward':
+        model = cluster.AgglomerativeClustering(n_clusters=n_clusters,
+                                                linkage='ward',
+                                                connectivity=connectivity)
+
+    elif algorithm=='AgglomerativeClustering':
+        model = cluster.AgglomerativeClustering(linkage="average",
+                                                affinity="cityblock",
+                                                n_clusters=n_clusters,
+                                                connectivity=connectivity)
+
+    model.fit(X)
+
+    if hasattr(model, 'labels_'):
+            y_pred = model.labels_.astype(np.int)
+    else:
+            y_pred = model.predict(X)
+
+    return X, y_pred
+
+
+
+def apply_cluster(data,algorithm,n_clusters,tools):
+
+    spectral = np.hstack([Spectral6] * 20)
+
+    x_cl, y_pred = clustering(data, algorithm, n_clusters)
+    colors = [spectral[i] for i in y_pred]
+
+    source = ColumnDataSource(data=dict(x=x_cl[:, 0], y=x_cl[:, 1], colors=colors))
+    plot = figure(toolbar_location=None, title=algorithm,tools=tools)
+    plot.circle('x', 'y', fill_color='colors', line_color=None, source=source)
+
+    
+    return source, plot, spectral
+
+
+def get_cluster_algorithms():
+
+    return [
+        'MiniBatchKMeans',
+        'AffinityPropagation',
+        'MeanShift',
+        'SpectralClustering',
+        'Ward',
+        'AgglomerativeClustering',
+        'DBSCAN',
+        'Birch'
+    ]
+
+
+    
+
--- bokeh_vis/main.py
+++ bokeh_vis/main.py
@@ -4,11 +4,11 @@ from bokeh.models import ColumnDataSource, HoverTool
 from data_format import get_data
 from pprint import pprint
 import numpy as np
-from bokeh.models.widgets import Select
+from bokeh.models.widgets import Select, Slider
 from sklearn.decomposition import PCA
 import random
 from bokeh.layouts import row, widgetbox, column
-
+import clustering
 
 def get_selections(keys):
     
@@ -32,7 +32,7 @@ def get_lstmGate_source(lstm,gate,data):
     return source
 
 
-def update_gate_source(attrname, old, new):
+def update_source(attrname, old, new):
     
     layer_value = selections[0].value
     gate_value  = selections[1].value
@@ -44,9 +44,30 @@ def update_gate_source(attrname, old, new):
     x_pca = pca.fit_transform(np.transpose(x))
     
     source.data = dict(x=x_pca[:,0],y=x_pca[:,1],z=np.array([i for i in range(x_pca.shape[0])]))
-    
-    
 
+    #update clustering source
+    algorithm = algorithm_select.value
+    n_clusters = int(cluster_slider.value)
+    x_cl, y_pred = clustering.clustering(x, algorithm, n_clusters)
+    colors = [cl_spectral[i] for i in y_pred]
+    cluster_plot.title.text = algorithm
+    cluster_source.data = dict(colors=colors, x=x_cl[:, 0], y=x_cl[:, 1])
+
+    
+    
+def update_clustering(attrname,old,new):
+
+    algorithm = algorithm_select.value
+    n_clusters = int(cluster_slider.value)
+
+    x_cl, y_pred = clustering.clustering(data_cl, algorithm, n_clusters)
+    colors = [cl_spectral[i] for i in y_pred]
+
+    cluster_plot.title.text = algorithm
+    cluster_source.data = dict(colors=colors, x=x_cl[:, 0], y=x_cl[:, 1])
+
+
+    
 #Get trained model parameters: weights and gate values
 keys,data = get_data("/home/yannis/Desktop/tRustNN/bokeh_vis/data/model.json")
 
@@ -61,11 +82,23 @@ hover.mode = 'mouse'
 
 tools = "pan,wheel_zoom,box_zoom,reset,hover"
 p = figure(title="PCA scatter plot",tools=tools)
-p.scatter((source.data['x']), (source.data['y']), marker='circle', size=10,line_color="navy", fill_color="navy", alpha=0.5)
+p.scatter((source.data['x']), (source.data['y']), marker='circle', size=10, line_color="navy", fill_color="navy", alpha=0.5)
 
 for attr in selections:
-    attr.on_change('value', update_gate_source)
+    attr.on_change('value', update_source)
 
 
-curdoc().add_root(row(inputs, p, width=400))
+#Clustering
+
+data_cl = data[selections[0].value][selections[1].value]
+cluster_source, cluster_plot, cl_spectral = clustering.apply_cluster(data_cl,'MiniBatchKMeans',2,tools=tools)
+
+algorithm_select = Select(value='MiniBatchKMeans',title='Select algorithm:',width=200, options=clustering.get_cluster_algorithms())
+cluster_slider = Slider(title="Number of clusters",value=2.0,start=2.0,end=10.0,step=1,width=400)
+
+algorithm_select.on_change('value', update_clustering)
+cluster_slider.on_change('value',  update_clustering)
+
+
+curdoc().add_root(row(inputs, p, algorithm_select, cluster_slider, cluster_plot, width=400))
 curdoc().title = "tRustNN"
